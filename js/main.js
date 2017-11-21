e1261338d184c2d0f104bad387c9e9fb0b48f962
Samuel Lindsey <samuel.lindsey@icloud.com>
2017-11-20 21:10:49 -0500

    added validation to email field and style modal

--- js/main.js
+++ js/main.js
@@ -32,3 +32,26 @@ function outsideClick(e) {
     modal.style.display = 'none';
   }
 }
+
+
+// Script to validate email and phone number
+
+// Set vars for email vailidate
+var form = document.getElementsByTagName('form')[0];
+var email = document.getElementById('email');
+var error = document.querySelector('.error');
+
+email.addEventListener('input', function (event) {
+  if (email.validity.valid) {
+    error.innerHTML = "";
+    error.className = "error";
+  }
+}, false);
+
+form.addEventListener("submit", function (event) {
+  if (!email.validity.valid) {
+    error.innerHTML = "Please enter a valid address.";
+    error.className = "error active";
+    event.preventDefault();
+  }
+},false);
